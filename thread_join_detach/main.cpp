39dcc6750ac829f75a5de4d9c23548fe2ecfa71c
siexp <elioni17@gmail.com>
2019-09-17 15:10:19 +0300

    join-join; detach-join

--- thread_join_detach/main.cpp
+++ thread_join_detach/main.cpp
@@ -6,9 +6,43 @@ void job()
     std::cout << "Hello, World!" << std::endl;
 }
 
-int main (int argc, char *argv[])
+void twice_join_test()
 {
-    std::thread t1{ job };
-    
+    std::thread t2{job};
+
+    t2.join();
+    t2.join();        
+}
+
+void join_detached_test()
+{
+    std::thread t3{job};
+    t3.detach();
+    t3.join();
+}
+
+int main(int argc, char *argv[])
+{
+    // calls std::terminate because thread is not joined
+    // std::thread t1{ job };
+
+    try
+    {
+        twice_join_test();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    try
+    {
+        join_detached_test();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
     return 0;
-}	
+}
